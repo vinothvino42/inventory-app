735dd6ea80a58b20043865681f5632033ff85a0e
vinothvino42 <vinothvino42@gmail.com>
2025-12-22 06:55:38 +0530

    feat: add sign-in page user interface

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from "@stackframe/stack";
+import Link from "next/link";
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
+      <div className="max-w-md w-full flex flex-col items-center space-y-8">
+        <SignIn />
+        <Link href={"/"}>Go Back Home</Link>
+      </div>
+    </div>
+  );
+}
